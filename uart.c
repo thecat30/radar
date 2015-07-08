fab5b2aeddbf4c4ed02536984585a5a3fe96e55b
redspirit34 <marshall260@gmail.com>
2015-07-08 08:18:04 +0200

    f
    f

---
+++ uart.c
@@ -0,0 +1,89 @@
+#include "uart.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <delays.h>
+#include <pic18f45k20.h>
+
+void UART_Init()
+{
+        //      BAUDCON: BAUD RATE CONTROL REGISTER           //
+        BRG16 = 0; // 8-bit Baud Rate Generator is used
+        SPBRG = 25; // Baudrate config
+        BRGH = 1;
+        //      TXSTA: TRANSMIT STATUS AND CONTROL REGISTER   //
+        TX9 = 0;   // Selects 8-bit transmission
+        TXEN = 1;  // Transmit enabled
+        SYNC = 0;  // Asynchronous mode
+        //     RCSTA: RECEIVE STATUS AND CONTROL REGISTER     //
+        RX9 = 0;   // Selects 8-bit reception
+        CREN = 1;  // Enables receiver
+        SPEN = 1;  // Serial port enabled
+        TRISC7 = 1;// RX
+        TRISC6 = 1;// TX
+        RCIF =1;
+}
+
+void DataTransfert(unsigned int distance)
+{   
+    char buffer;
+    char number[4];
+    sprintf(number, "%d", distance);
+    if(ready==1)
+    {
+        UART_Write_Text(longueur); 
+        UART_Write_Text(number);
+        UART_Write_Text(backspace);  
+    }
+    Delay10KTCYx(100);
+    
+    buffer = UART_Read();
+    if(buffer!='0') {
+        if(buffer=='o')
+        UART_Write_Text(gone);
+        else if(buffer=='n')
+        UART_Write_Text(stop);
+                
+        switch(buffer) {
+            case 'o' :
+                ready = 1;
+                break;
+            case 'n' :
+                ready = 0;
+                break;  
+            
+        }
+    }
+    
+}
+
+char UART_TX_Empty()
+{
+  return TRMT;
+}
+char UART_Data_Ready()
+{
+   return RCIF;
+}
+char UART_Read()
+{
+  if(RCIF)
+    return RCREG;
+  else
+    return 0;
+}
+void UART_Read_Text(char *Output, unsigned int length)
+{
+	for(int i=0;i<length;i++)
+		Output[i] = UART_Read();
+}
+void UART_Write(char data)
+{
+  while(!TRMT);
+  TXREG = data;
+}
+void UART_Write_Text(char *text)
+{
+  int i;
+  for(i=0;text[i]!='\0';i++)
+	  UART_Write(text[i]);
+}
---
+++ uart.h
@@ -0,0 +1,28 @@
+/* 
+ * File:   uart.h
+ * Author: Rémi Monge
+ *
+ * Created on 8 juillet 2015, 07:51
+ */
+#ifndef UART_H
+#define	UART_H
+
+
+int ready = 1;
+char stop[] = "Sonar Stop\n\r";
+char gone[] = "Sonar Play\n\r";
+char backspace[] = "cm\n\r";
+char longueur[] = "Longueur : ";  
+
+
+extern void UART_Init(void);
+extern void DataTransfert(unsigned int);
+extern char UART_TX_Empty();
+extern char UART_Data_Ready();
+extern char UART_Read();
+extern void UART_Read_Text(char *Output, unsigned int length);
+extern void UART_Write(char data);
+extern void UART_Write_Text(char *text);
+
+
+#endif
